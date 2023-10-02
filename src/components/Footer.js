190970f7eaa239c60fdeb9d53d27c9bc3a9a7609
rob <rob_abcba@yahoo.com>
2023-10-02 08:08:30 -0700

    First create Header and Footer components that will be in every page.  Add to App.js with Bootstrap 5.3 classNames to test Bootstrap CDN links.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-warning">
+      <h1>Footer.js</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-primary">
+      <h1>Header.js</h1>
+    </div>
+  );
+};
+
+export default Header;
